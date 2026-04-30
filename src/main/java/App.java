cb6dc0492962c2bb7db08848609acb5be0ca9bdf
Rasha K <rashakrasha4@gmail.com>
2026-04-30 11:38:44 +0530

    add html output

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,13 @@
-public class App {
+import java.io.FileWriter;
+
+class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        try {
+            FileWriter file = new FileWriter("output.html");
+            file.write("<h1>Hello Jenkins</h1>");
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
